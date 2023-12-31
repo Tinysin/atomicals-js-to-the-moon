7f55777ac7a0345ce0ddd3a9649253c0a8e58081
kai <kai@kmbp.local>
2023-12-31 21:44:32 +0800

    open half cpu cores

---
+++ loop-mint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Specify the number of loops
+numberOfLoops=10
+
+# Specify the output file
+outputFile="log.txt"
+
+# Use a for loop to execute a specified number of times
+for ((i=1; i<=numberOfLoops; i++))
+do
+    echo "This is the ${i}th loop" | tee -a $outputFile
+
+    # Run yarn command and append output to the file
+    yarn cli mint-dft quark --satsbyte 100 >> $outputFile 2>&1
+
+    # Check whether the command was executed successfully
+    if [ $? -ne 0 ]; then
+        echo "Command execution failed, try to run again..." | tee -a $outputFile
+    else
+        echo "The command was executed successfully and is ready for the next cycle..." | tee -a $outputFile
+    fi
+
+    # Wait for some time (optional)
+    sleep 1
+done
+
+echo "loop completed" | tee -a $outputFile
